8ed77e2363e4c152c96582d137b3860245e2da03
DESKTOP-5EILI15\dexPC <t.kolesnik>
2024-02-28 09:49:32 +0200

    Translator

---
+++ Translator/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+
+var text = "ёбанный в рот того казино блять";
+
+Console.WriteLine("Было: " + text);
+
+text = Translate(text);
+
+Console.WriteLine("Стало: " + text);
+
+string Translate(string text)
+{
+    var textListSymbol = text.ToCharArray();
+
+    var textBuilder = new StringBuilder();
+
+    foreach (var item in textListSymbol)
+    {
+        var convertSymbol = Convert(item.ToString());
+        textBuilder.Append(convertSymbol);
+    }
+
+    return textBuilder.ToString();
+}
+
+string Convert(string symbol)
+{
+    var keyValueLanguage = new Dictionary<string, string>()
+    {
+        { "а", "a" },
+        { "б", "b" },
+        { "в", "v" },
+        { "г", "g" },
+        { "д", "d" },
+        { "е", "e" },
+        { "ё", "yo" },
+        { "ж", "zh" },
+        { "з", "z" },
+        { "и", "i" },
+        { "й", "i" },
+        { "к", "k" },
+        { "л", "l" },
+        { "м", "m" },
+        { "н", "n" },
+        { "о", "o" },
+        { "п", "p" },
+        { "р", "r" },
+        { "с", "s" },
+        { "т", "t" },
+        { "у", "u" },
+        { "ф", "f" },
+        { "х", "h" },
+        { "ц", "ts" },
+        { "ч", "ch" },
+        { "ш", "sh" },
+        { "щ", "shch" },
+        { "ъ", "" },
+        { "ы", "y" },
+        { "ь", "" },
+        { "э", "e" },
+        { "ю", "yu" },
+        { "я", "ya" }
+    };
+
+    var convertValue = keyValueLanguage.FirstOrDefault(x => x.Value == symbol.ToLower()).Key;
+
+    if (convertValue == null)
+    {
+        convertValue = keyValueLanguage.FirstOrDefault(x => x.Key == symbol.ToLower()).Value ?? symbol;
+    }
+
+    return convertValue;
+}
